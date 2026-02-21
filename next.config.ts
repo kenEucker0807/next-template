2d3305e08d71755c16b8a66e339f12379a7e15c3
kenEucker0807 <kenEucker0807@outlook.com>
2026-02-21 15:19:51 -0700

    Revert "Fix build error."

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  sassOptions: {
-    includePaths: [path.join(__dirname, "src/styles")],
-  },
 };
 
 export default nextConfig;
